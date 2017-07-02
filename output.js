955ddd7d64d9190177046249179a916de514ea1b
Trevor Lang <trevorsaynever@gmail.com>
2017-07-02 15:15:53 -0400

    Tweaking hostname

--- output.js
+++ output.js
@@ -7,14 +7,12 @@ function Output(location) {
   this.pin = new Gpio(location, 'out');
 
   this.on = function() {
-    console.log(`Led on`);
     this.pin.write( 1, function() {
   		console.log(`Led on`);
   	});
   }
 
   this.off = function() {
-    console.log(`Led off`);
     this.pin.write( 0, function() {
   		console.log(`Led off`);
   	});
